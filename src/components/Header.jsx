e22854f55801752819340436847948312d094165
RodrigoUbuntu4070 <rtf.franca@gmail.com>
2024-06-29 14:45:33 -0300

    Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../index.css'
+
+export default function Header() {
+  return (
+    <div className='header'>
+        <h1>Fast React Pizza Co.</h1>
+    </div>
+  )
+}
