0e7d5635df1a0dae8607bcf99e6687a8f2fe4708
RodrigoUbuntu4070 <rtf.franca@gmail.com>
2024-06-29 15:09:31 -0300

    Conditional styling of Pizzas Img with ternary operator

--- src/components/PizzasList.jsx
+++ src/components/PizzasList.jsx
@@ -14,13 +14,13 @@ export default function PizzasList() {
 
 function PizzaItem({ pizzaObj }) {
   return (
-    <div className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt="" />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
         <span>{pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
