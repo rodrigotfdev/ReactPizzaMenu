dab0e270c4b57030d3d4bb953bc1525e2293fab1
RodrigoUbuntu4070 <rtf.franca@gmail.com>
2024-06-29 14:48:35 -0300

    Top Menu Component

---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../index.css";
+
+export default function Menu() {
+  return (
+    <div className="menu">
+      <h2>Our Menu</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, dicta corporis aut quo ipsam magnam doloremque ex quos labore. Harum quas reprehenderit dolor ab nulla obcaecati recusandae aliquam, adipisci perferendis?</p>
+    </div>
+  );
+}
